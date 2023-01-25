f9bb296d7a2ce0a6af2b75f96a1d974c3dc70efe
alysedelaney <114497259+alysedelaney@users.noreply.github.com>
2023-01-24 20:33:45 -0500

    Create pets.py

---
+++ week_2/pets.py
@@ -0,0 +1,29 @@
+pets_i_know = ['Mia', 'Olive', 'Lex', 'Frida', 'Ajax', 'Jensen', 'Sirius', 'Tippy', 'Chippy', 'Libby']
+
+pets = {
+    'Mia': {'type':'Dog', 'age':4},
+    'Olive': {'type':'Cat', 'age':3},
+    'Lex': {'type':'Cat', 'age':8},
+    'Frida': {'type':'Dog', 'age':1},
+    'Ajax': {'type':'Cat', 'age':5},
+    'Jensen': {'type':'Cat', 'age':6},
+    'Sirius': {'type':'Cat', 'age':3},
+    'Tippy': {'type':'Rabbit', 'age':8},
+    'Chippy': {'type':'Rabbit', 'age':4},
+    'Libby': {'type':'Rabbit', 'age':3},
+}
+
+pets = [
+    {'name':'Mia', 'type':'Dog', 'age': 4},
+    {'name':'Olive', 'type':'Cat', 'age': 3},
+    {'name':'Lex', 'type':'Cat', 'age': 8},
+    {'name':'Frida', 'type':'Dog', 'age': 1},
+    {'name':'Ajax', 'type':'Cat', 'age': 5},
+    {'name':'Jensen', 'type':'Cat', 'age': 6},
+    {'name':'Sirius', 'type':'Cat', 'age': 3},
+    {'name':'Tippy', 'type':'Rabbit', 'age': 8},
+    {'name':'Chippy', 'type':'Rabbit', 'age': 4},
+    {'name':'Libby', 'type':'Rabbit', 'age': 3},
+]
+
+print(pets)
