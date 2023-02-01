b4a6df2c2b86949de260683d2b9c051b3f548b55
alysedelaney <114497259+alysedelaney@users.noreply.github.com>
2023-01-31 20:50:49 -0500

    in class work

---
+++ inclass_tests/functions.py
@@ -0,0 +1,19 @@
+# a_string = "hello world"
+# print(a_string.upper())
+
+# def hello(a): 
+#     print("hello")
+#     print(a*12)
+
+# hello(23213)
+
+my_list = [1,2,3,4,5]
+
+def add_2(val): 
+    new_val = val + 2
+    return new_val
+
+for i in my_list: 
+    add_2(i)
+
+print(my_list)
